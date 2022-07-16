d174753ac94052e8495e1ba39f76fc70e18a1a22
Keith <keithwesley187@gmail.com>
2022-07-16 09:01:12 +0300

    Done.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inTheCart, setInTheCart] = useState(false)
+
+  function handleClick(){
+    setInTheCart((inTheCart) => !inTheCart)
+  }
   return (
-    <li className="">
+    <li className={inTheCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>{inTheCart ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [filterD, setFilter] = useState("All")
+  const [theChosen, setChosen] = useState(items)
+
+  function handleFilter(e){
+    setFilter(e.target.value)
+  }
+
+  const mashakura = theChosen.filter((shakura) => {
+    if(filterD === "All"){
+      return true
+    } else {
+      return shakura.category === filterD
+    }
+  })
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleFilter}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +27,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {mashakura.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
